8e47303cff5bc10ebc2af530533bf81eaca9e837
sarah1234-lab <es-SarahMohamed2025@alexu.edu.eg>
2026-04-09 19:09:51 +0200

    first commit

---
+++ src/test/java/com/Sarah/travel_planner/TravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sarah.travel_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
